6d11ba5411e26d9cf61105784ded33b5b10c5feb
Kamil Biernat <k.biernat@yahoo.ca>
2022-12-09 16:49:10 +0100

    getting data from backend

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from './model/product'
 
 @Injectable({
@@ -6,34 +8,35 @@ import { Product } from './model/product'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getProducts(): Product[] {
-    return [
-      {
-        name: "Makrama 1",
-        category: "Makramy",
-        img: "/assets/bor.jpg",
-        description: "Makrama w rozmiarze XL, kolor bordowy",
-        price: 299.00,
-        currency: "PLN"
-      },
-      {
-        name: "Makrama 2",
-        category: "Makramy",
-        img: "/assets/bez.jpeg",
-        description: "Makrama w rozmiarze L, kolor beżowy",
-        price: 259.00,
-        currency: "PLN"
-      },
-      {
-        name: "Makrama 3",
-        category: "Makramy",
-        img: "/assets/cz.jpg",
-        description: "Makrama w rozmiarze M, kolor czarny",
-        price: 199.00,
-        currency: "PLN"
-      }
-    ];
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>("/api/products");
+    // return [
+    //   {
+    //     name: "Makrama 1",
+    //     category: "Makramy",
+    //     img: "/assets/bor.jpg",
+    //     description: "Makrama w rozmiarze XL, kolor bordowy",
+    //     price: 299.00,
+    //     currency: "PLN"
+    //   },
+    //   {
+    //     name: "Makrama 2",
+    //     category: "Makramy",
+    //     img: "/assets/bez.jpeg",
+    //     description: "Makrama w rozmiarze L, kolor beżowy",
+    //     price: 259.00,
+    //     currency: "PLN"
+    //   },
+    //   {
+    //     name: "Makrama 3",
+    //     category: "Makramy",
+    //     img: "/assets/cz.jpg",
+    //     description: "Makrama w rozmiarze M, kolor czarny",
+    //     price: 199.00,
+    //     currency: "PLN"
+    //   }
+    // ];
   }
 }
