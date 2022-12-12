984bc67808b90e2600185237790439f6a5b3f2d8
Kamil Biernat <k.biernat@yahoo.ca>
2022-12-12 12:57:15 +0100

    product list with pagination in admin panel

---
+++ src/app/modules/admin/admin-product/admin-product.component.ts
@@ -0,0 +1,32 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { map, startWith, switchMap } from 'rxjs';
+import { AdminProduct } from './admin-product';
+import { AdminProductService } from './admin-product.service';
+
+@Component({
+  selector: 'app-admin-product',
+  templateUrl: './admin-product.component.html',
+  styleUrls: ['./admin-product.component.scss']
+})
+export class AdminProductComponent implements AfterViewInit{
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  displayedColumns: string[] = ["id", "name", "price"]
+  totalElements: number = 0;
+  dataSource: AdminProduct[] = [];
+
+  constructor(private adminProductService: AdminProductService) {}
+
+  ngAfterViewInit(): void {
+    this.paginator.page.pipe(
+      startWith({}),
+      switchMap(() => {
+        return this.adminProductService.getProducts(this.paginator.pageIndex, this.paginator.pageSize);
+      })
+    ).subscribe(dataSource => {
+      this.totalElements = dataSource.totalElements;
+      this.dataSource = dataSource.content;
+    });
+  }
+}
