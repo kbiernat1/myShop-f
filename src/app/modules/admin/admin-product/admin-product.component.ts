b3dd817a6d790d0e825e79efe53745645216b44d
Kamil Biernat <k.biernat@yahoo.ca>
2022-12-19 12:46:30 +0100

    product delete from admin panel

--- src/app/modules/admin/admin-product/admin-product.component.ts
+++ src/app/modules/admin/admin-product/admin-product.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { map, startWith, switchMap } from 'rxjs';
+import { MatTable } from '@angular/material/table';
+import { startWith, switchMap } from 'rxjs';
+import { AdminConfirmDialogService } from '../admin-confirm-dialog.service';
 import { AdminProduct } from './admin-product';
 import { AdminProductService } from './admin-product.service';
 
@@ -12,11 +14,15 @@ import { AdminProductService } from './admin-product.service';
 export class AdminProductComponent implements AfterViewInit{
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatTable) table!: MatTable<any>;
+
   displayedColumns: string[] = ["id", "name", "price", "actions"]
   totalElements: number = 0;
   dataSource: AdminProduct[] = [];
 
-  constructor(private adminProductService: AdminProductService) {}
+  constructor(
+    private adminProductService: AdminProductService,
+    private dialogService: AdminConfirmDialogService) {}
 
   ngAfterViewInit(): void {
     this.paginator.page.pipe(
@@ -27,6 +33,24 @@ export class AdminProductComponent implements AfterViewInit{
     ).subscribe(dataSource => {
       this.totalElements = dataSource.totalElements;
       this.dataSource = dataSource.content;
+    });
+  }
+
+  confirmDelete(element: AdminProduct) {
+    this.dialogService.openConfirmDialog("Czy na pewno chcesz usunąć produkt?")
+    .afterClosed()
+    .subscribe(result => {
+      if(result) {
+        this.adminProductService.delete(element.id)
+        .subscribe(() => {
+          this.dataSource.forEach((value, index) => {
+            if(element == value) {
+              this.dataSource.splice(index, 1);
+              this.table.renderRows();
+            }
+          })
+        });
+      }
     });
   }
 }
