7e3c4291cae5141a1c7167e21b68fbf055443e39
Kamil Biernat <k.biernat@yahoo.ca>
2023-01-04 15:45:36 +0100

    cart update; icon of products am. in cart

--- src/app/modules/cart/cart.service.ts
+++ src/app/modules/cart/cart.service.ts
@@ -7,14 +7,22 @@ import { CartSummary } from './model/cartSummary';
   providedIn: 'root'
 })
 export class CartService {
-
+    
   constructor(private http: HttpClient) { }
-
+  
   getCart(id: number): Observable<CartSummary> {
-    return this.http.get<CartSummary>("api/carts/" + id);
+    return this.http.get<CartSummary>("/api/carts/" + id);
+  }
+  
+  addToCart(id: number, cartItem: any): Observable<CartSummary> {
+    return this.http.put<CartSummary>("/api/carts/" + id, cartItem);
+  }
+  
+  updateCart(id: number, items: any[]): Observable<CartSummary> {
+    return this.http.put<CartSummary>("/api/carts/" + id + "/update", items);
   }
 
-  addToCart(id: number, cartItem: any): Observable<CartSummary> {
-    return this.http.put<CartSummary>("api/carts/" + id, cartItem);
+  deleteCartItem(itemId: number): Observable<void> {
+    return this.http.delete<void>("/api/cartItems/" + itemId)
   }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { CartIconService } from 'src/app/modules/common/service/cart-icon.service';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title = "Kamilla with magic"
+  title = "Kamilla with magic";
+  cartProductCounter = "";
 
-  constructor() { }
+  constructor(
+    private cookieService: CookieService,
+    private headerService: HeaderService,
+    private cartIconService: CartIconService
+  ) { }
 
   ngOnInit(): void {
-    
+    this.getCountProducts();
+    this.cartIconService.subject
+    .subscribe(counter => this.cartProductCounter = String(counter>0 ? counter : ""))
   }
 
+  getCountProducts() {
+    this.headerService.getCountProducts(Number(this.cookieService.get("cartId")))
+    .subscribe(counter => this.cartProductCounter = String(counter>0 ? counter : ""));
+  }
 }
